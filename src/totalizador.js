2ed83f8ca570c648ff48761ea12f8b3c75808631
Carlitos <carlos.chavez.s@ucb.edu.bo>
2026-09-09 18:33:51 -0400

    agregar descuento

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,3 +1,11 @@
 export function totalNeto(cantidad, precio) {
   return cantidad * precio;
 }
+
+export function descuento(total) {
+  if (total >= 1000) {
+    return total * 0.03;
+  }
+
+  return 0;
+}
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -1,7 +1,12 @@
-import { totalNeto } from "./totalizador";
+import { totalNeto, descuento } from "./totalizador";
 
 describe("Totalizador de venta", () => {
   test("calcula el precio neto de la compra", () => {
     expect(totalNeto(20, 3)).toBe(60);
   });
+
+  test("aplica 3% de descuento cuando la compra es de 1000", () => {
+    expect(descuento(1000)).toBe(30);
+  });
+
 });
